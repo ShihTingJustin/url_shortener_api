b94f6c74637ee55bbdef96173f7feb138cda4032
Justin Huang <justinhuang777@gmail.com>
2020-11-21 16:26:42 +0800

    add helper function and test case

---
+++ helpers.js
@@ -0,0 +1,28 @@
+const Url = require('./models/url')
+const axios = require('axios')
+const { nanoid } = require('nanoid')
+
+module.exports = {
+  isOriginalUrlValid: async (url) => {
+    try {
+      const res = await axios.get(url)
+      return (res.status === 200) ? true : false
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  createShortUrl: async (length) => {
+    return await nanoid(length)
+  },
+
+  isShortUrlUnique: async (shortUrl) => {
+    try {
+      const checkShortUrl = await Url.findOne({ shortUrl }).exec()
+      if (!checkShortUrl) return shortUrl
+      else isShortUrlUnique(createShortUrl())
+    } catch (err) {
+      console.log(err)
+    }
+  },
+}
---
+++ test/test.js
@@ -0,0 +1,29 @@
+const Url = require('../models/url')
+const helpers = require('../helpers')
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../app')
+
+describe('helper function test', () => {
+  describe('Is original url valid', () => {
+    it('should return true when url is valid', async () => {
+      assert.strictEqual(await helpers.isOriginalUrlValid('https://www.apple.com/tw/'), true)
+    })
+  })
+
+  describe('Create 5 digit short url', () => {
+    it('length of short url should be 5', async () => {
+      const shortUrl = await helpers.createShortUrl(5)
+      assert.strictEqual(shortUrl.length, 5)
+    })
+  })
+
+  describe('Is short url unique', () => {
+    it('should return short url when it is unique', async () => {
+      assert.strictEqual(await helpers.isShortUrlUnique('12345'), '12345')
+    })
+  })
+})
+
+
+
