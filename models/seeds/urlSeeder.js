23858d47aabdf66880c441fe6052e0820c24022e
Justin Huang <justinhuang777@gmail.com>
2021-03-19 20:42:18 +0800

    update seed data and urlSeeder.js

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,30 +1,12 @@
 const Url = require('../url')
 const db = require('../../config/mongoose')
-const helpers = require('../../helpers')
+const seedData = require('./seed.json')
 
-const originalUrls = [
-  'https://www.apple.com/tw/',
-  'https://www.apple.com/tw/mac/',
-  'https://www.apple.com/tw/mac-pro/',
-  'https://www.apple.com/tw/imac/',
-  'https://www.apple.com/tw/macbook-pro-16/',
-  'https://www.apple.com/tw/macbook-pro-13/',
-  'https://www.apple.com/tw/macbook-air/',
-  'https://www.apple.com/tw/ipad/',
-  'https://www.apple.com/tw/iphone/',
-  'https://www.apple.com/tw/watch/'
-]
+const records = seedData.map(item => JSON.parse(JSON.stringify(item)))
 
 db.once('open', async () => {
   console.log('run seeder...')
-  const shortUrlLength = 5
-  for (let i = 0; i < 10; i++) {
-    await Url.create({
-      originalUrl: originalUrls[i],
-      shortUrl: `${await helpers.isShortUrlUnique(await helpers.createShortUrl(shortUrlLength))}`,
-      click: i
-    })
-  }
+  await Url.create(...records)
   console.log('seeder complete!')
   process.exit()
 })
