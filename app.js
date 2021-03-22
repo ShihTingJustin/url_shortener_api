822aaab25da5a555c500fceab03d5298c205e62a
Justin Huang <justinhuang777@gmail.com>
2021-03-22 22:42:02 +0800

    remove socket.io

--- app.js
+++ app.js
@@ -18,30 +18,7 @@ app.use(methodOverride('_method'))
 app.use(routes)
 swaggerDoc(app)
 
-var server = require("http").createServer(app);
-var socket = require("socket.io")(server, {
-  allowEIO3: true,
-  cors: {
-    origin: process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'http://localhost:8080',
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-});
-
-socket.on("connection", (socket) => {
-  if (socket.connected) {
-    console.log("[socket] client is connected");
-    //socket.emit("shortUrlClick", { shortUrlClick: 63, id: "6052eb3d13941a4871603fcd"});
-  }
-  socket.on("connect", () => {
-    console.log("client is connected");
-  });
-  socket.on("disconnect", () => {
-    console.log("client is disconnected");
-  });
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Express is listening on localhost:${PORT}`)
 })
 
